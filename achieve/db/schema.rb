645c77a4112371abcd445a2993b30263cd7f6eda
ponpppher <genmaier@gmail.com>
2018-10-22 18:05:50 +0900

    Userモデル追加

--- achieve/db/schema.rb
+++ achieve/db/schema.rb
@@ -10,13 +10,33 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_24_101359) do
+ActiveRecord::Schema.define(version: 2018_10_22_083702) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "lists", force: :cascade do |t|
-    t.text "content"
+  create_table "favorites", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "list_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["list_id"], name: "index_favorites_on_list_id"
+    t.index ["user_id"], name: "index_favorites_on_user_id"
   end
 
+  create_table "lists", force: :cascade do |t|
+    t.text "content"
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_lists_on_user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_foreign_key "favorites", "lists"
 end
