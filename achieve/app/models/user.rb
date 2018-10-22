fe99baaaa7436329d3efabb740d15ee865489d23
ponpppher <genmaier@gmail.com>
2018-10-22 19:04:15 +0900

    ユーザー登録フォーム作成、新規作成機能追加

--- achieve/app/models/user.rb
+++ achieve/app/models/user.rb
@@ -5,4 +5,5 @@ class User < ApplicationRecord
   uniqueness: true,
   format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
   has_secure_password
+  validates :password, presence: true, length: { minimum:6 }
 end
--- achieve/config/routes.rb
+++ achieve/config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
       post :confirm
     end
   end
+
+  resources :users, only:[:new, :create]
   #get '/lists', to: 'lists#index'
   #get 'lists/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
