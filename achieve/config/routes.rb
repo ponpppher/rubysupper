2e3a3c8b73ee0356dbef2ab4aabe8c74bdd5130e
ponpppher <genmaier@gmail.com>
2018-10-22 16:57:19 +0900

    新規投稿機能、投稿一覧機能、削除機能追加

--- achieve/config/routes.rb
+++ achieve/config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
-
-  resources :lists
+  root "lists#top.html"
+  resources :lists do
+    collection do
+      post :confirm
+    end
+  end
   #get '/lists', to: 'lists#index'
   #get 'lists/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
