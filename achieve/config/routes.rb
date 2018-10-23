090d59393d365a46bf82ead3d04ed99bfee4203c
ponpppher <genmaier@gmail.com>
2018-10-23 13:21:40 +0900

    make log in form

--- achieve/config/routes.rb
+++ achieve/config/routes.rb
@@ -7,7 +7,6 @@ Rails.application.routes.draw do
   end
 
   resources :users, only:[:new, :create, :show]
-  #get '/lists', to: 'lists#index'
-  #get 'lists/index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :sessions, only:[:new, :create, :destroy]
 end
