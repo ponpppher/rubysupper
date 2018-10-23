7f0c3310ab2d98013b814d77cbae86835999759e
ponpppher <genmaier@gmail.com>
2018-10-23 13:44:40 +0900

    add login logic

--- achieve/app/controllers/sessions_controller.rb
+++ achieve/app/controllers/sessions_controller.rb
@@ -1,4 +1,19 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    # search exist user from email
+    user = User.find_by(params[:session][:email].downcase)
+
+    # confirm user exist and password authenticate
+    if user && user.authentecate(params[:session][:password])
+      # move to log in page
+      redirect_to sessions_new_path
+    else
+      # display message
+      flash.now[:danger] = "login failed"
+      # display login page
+      redner 'new'
+    end
 end
