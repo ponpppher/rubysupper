6644f69865a5a1415953df62bc5886521121b68a
ponpppher <genmaier@gmail.com>
2018-10-29 14:23:10 +0900

    新規投稿画面作成

--- imagefeed/app/controllers/feeds_controller.rb
+++ imagefeed/app/controllers/feeds_controller.rb
@@ -1,4 +1,19 @@
 class FeedsController < ApplicationController
   def index
   end
+
+  def new
+    @feed = Feed.new
+  end
+
+  def create
+    Feed.create(feed_params)
+
+    redirect_to new_feed_path
+  end
+
+private
+  def feed_params
+    params.require(:feed).permit(:content)
+  end
 end
