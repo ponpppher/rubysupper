379a23045d6b124c38936530e48e180a77273e37
ponpppher <genmaier@gmail.com>
2018-10-29 17:33:05 +0900

    削除、編集機能追加

--- imagefeed/app/controllers/feeds_controller.rb
+++ imagefeed/app/controllers/feeds_controller.rb
@@ -1,4 +1,5 @@
 class FeedsController < ApplicationController
+before_action :set_feed, only:[:edit, :update, :destroy]
   def index
     @feed = Feed.all.reverse_order
   end
@@ -10,11 +11,31 @@ class FeedsController < ApplicationController
   def create
     Feed.create(feed_params)
 
-    redirect_to new_feed_path
+    redirect_to feeds_path 
+  end
+
+  def edit
+  end
+
+  def update
+    if @feed.update(feed_params)
+      redirect_to feeds_path, notice:"edit feed done"
+    else
+      redirect_to 'edit'
+    end
+  end
+
+  def destroy
+    @feed.destroy
+    redirect_to feeds_path
   end
 
 private
   def feed_params
     params.require(:feed).permit(:content)
+  end
+
+  def set_feed
+    @feed = Feed.find(params[:id])
   end
 end
