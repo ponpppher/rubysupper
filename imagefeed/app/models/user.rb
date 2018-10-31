2fe5699e357d87dd33e23997e6f6bd01c6878210
ponpppher <genmaier@gmail.com>
2018-10-31 17:20:51 +0900

    バリテーション設定追加ユニーク制約をdb側でも設定

--- imagefeed/app/models/user.rb
+++ imagefeed/app/models/user.rb
@@ -1,2 +1,16 @@
 class User < ApplicationRecord
+  # バリテーション前に小文字化させる
+  before_validation { email.downcase! }
+
+  # passwordのハッシュ値をpassword_digestカラムで保持
+  # autenticateメソッドを使用できるようになる
+  has_secure_password
+
+  # 空値禁止、30文字まで
+  validates :name, presence:true, length: {maximum: 30 }
+  # 空値禁止、255文字まで
+  # emailのフォーマット指定,重複禁止
+  validates :email, presence:true, length: {maximum: 255},
+    format:{with:/\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i},
+    uniqueness: true
 end
