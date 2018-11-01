e449dbb0bbc96290fd0a90d4c67864d0bd3c4c48
ponpppher <genmaier@gmail.com>
2018-11-01 14:10:18 +0900

    loginページ追加

--- imagefeed/config/routes.rb
+++ imagefeed/config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
   root "feeds#top.html"
   resources :feeds do
     collection do
@@ -7,4 +8,5 @@ Rails.application.routes.draw do
   end
 
   resources :users, only:[:new, :create, :show]
+  resources :sessions, only:[:new, :create, :destroy]
 end
