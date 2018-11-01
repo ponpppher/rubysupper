2d9bac8e9a1624584b256f1910add0f02397cadf
ponpppher <genmaier@gmail.com>
2018-11-01 14:51:13 +0900

    ログイン機能追加

--- imagefeed/app/controllers/sessions_controller.rb
+++ imagefeed/app/controllers/sessions_controller.rb
@@ -1,4 +1,21 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+  # 既存ユーザー検索
+    user = User.find_by(email: params[:session][:email].downcase)
+  # confirm user exist and password authenticate
+    if user && user.authenticate(params[:session][:password])
+      # set userid into session
+      session[:user_id] = user.id
+      # move to user detail page
+      redirect_to user_path(user.id)
+    else
+      # display message
+      flash.now[:danger] = 'login failed'
+      # display login page
+      render 'new'
+    end
+  end
 end
