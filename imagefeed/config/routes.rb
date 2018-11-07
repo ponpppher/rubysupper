e4e0242aaeb3c1b89da08d22a736325f4cd3801a
ponpppher <genmaier@gmail.com>
2018-11-08 05:48:59 +0900

    favoriteのルーティング設定

--- imagefeed/config/routes.rb
+++ imagefeed/config/routes.rb
@@ -8,5 +8,6 @@ Rails.application.routes.draw do
 
   resources :users, only:[:new, :create, :show]
   resources :sessions, only:[:new, :create, :destroy]
+  resources :favorites, only:[:create, :destroy]
   resources :likes, only:[:index]
 end
