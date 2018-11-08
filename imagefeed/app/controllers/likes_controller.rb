6d12493f913dfbc6363de88de072d2b4bf521daf
ponpppher <genmaier@gmail.com>
2018-11-08 23:22:40 +0900

    お気に入り削除機能追加

--- imagefeed/app/controllers/likes_controller.rb
+++ imagefeed/app/controllers/likes_controller.rb
@@ -3,8 +3,8 @@ class LikesController < ApplicationController
     @like = current_user.favorite_feed
   end
 
-#  def destroy
-#    favorite = current_user.favorites.find_by(feed_id: params[:id]).destroy
-#    redirect_to likes_url, notice: "del #{favorite.user.name}'s like"
-#  end
+  def destroy
+    favorite = current_user.favorites.find_by(feed_id: params[:id]).destroy
+    redirect_to likes_url, notice: "del #{favorite.user.name}'s like"
+  end
 end
--- imagefeed/config/routes.rb
+++ imagefeed/config/routes.rb
@@ -9,5 +9,5 @@ Rails.application.routes.draw do
   resources :users, only:[:new, :create, :show]
   resources :sessions, only:[:new, :create, :destroy]
   resources :favorites, only:[:create, :destroy]
-  resources :likes, only:[:index]
+  resources :likes, only:[:index, :destroy]
 end
