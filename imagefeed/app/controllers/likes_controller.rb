fd8c9a2851ef879f05e4bc36ed6fd742240519a7
ponpppher <genmaier@gmail.com>
2018-11-08 23:17:28 +0900

    お気に入り一覧ページ作成

--- imagefeed/app/controllers/likes_controller.rb
+++ imagefeed/app/controllers/likes_controller.rb
@@ -1,5 +1,10 @@
 class LikesController < ApplicationController
   def index
-    @like = current_user.favorites
+    @like = current_user.favorite_feed
   end
+
+#  def destroy
+#    favorite = current_user.favorites.find_by(feed_id: params[:id]).destroy
+#    redirect_to likes_url, notice: "del #{favorite.user.name}'s like"
+#  end
 end
